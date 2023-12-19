b575cd6179a90883526d8b2f8738b2f60bddfc28
Daniel Bellfield <dnlbellfield@gmail.com>
2023-12-18 21:29:55 -0800

    update store method in ChirpController

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -31,9 +31,16 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+      $validated = $request->validate([
+        'message' => 'required|string|max:255',
+    ]);
+
+    $request->user()->chirps()->create($validated);
+
+    return redirect(route('chirps.index'));
+    
     }
 
     /**
